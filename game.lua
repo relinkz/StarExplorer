946853660f8913689a37307ee86545fe71fac103
Sebastian Lundgren <seb.lundgren@gmail.com>
2020-02-19 07:15:11 +0100

    bluh

--- game.lua
+++ game.lua
@@ -3,6 +3,7 @@ local composer = require( "composer" )
 
 local scene = composer.newScene()
 
+-- random shit
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
